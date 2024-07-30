303ef4f07fbc3cab142e9db56cd5c2fc751aadc1
Yamunasri1810 <167014416+Yamunasri1810@users.noreply.github.com>
2024-07-30 23:18:09 +0530

    Update function.py

--- function.py
+++ function.py
@@ -32,6 +32,18 @@ def take_command():
 
 question = ["Who is", "Who might", "Who exactly is", "Who are", "What is", "What's", "Can you tell me about" ,"What can you tell me about" ,"Who’s that", "Could you explain", "Who’s", "Do you know who" ,"Who would that be", "Who’s behind", "Who’s the person named", "Whose identity is ", "Who goes by the name"]
 
+def increase_volume(percent=10):
+    """Increase the volume by the specified percentage"""
+    current_volume = stream.get_volume()
+    new_volume = min(1.0, current_volume + (percent / 100))
+    stream.set_volume(new_volume)
+
+def decrease_volume(percent=10):
+    """Decrease the volume by the specified percentage"""
+    current_volume = stream.get_volume()
+    new_volume = max(0.0, current_volume - (percent / 100))
+    stream.set_volume(new_volume)
+
 def run_nova():
     print("Hi I’m Nova. How can I help you?")
     while True:
@@ -55,6 +67,20 @@ def run_nova():
             print(jokes)
         elif 'goodbye' in command or 'good bye' in command:
             break
+        elif "increase" in command and "volume" in command:
+            percent = 10
+            if "by" in command:
+                percent = int(command.split("by")[1].split("%")[0])
+            increase_volume(percent)
+            print(f"Increased volume by {percent}%")
+        elif "decrease" in command and "volume" in command:
+            percent = 10
+            if "by" in command:
+                percent = int(command.split("by")[1].split("%")[0])
+            decrease_volume(percent)
+            print(f"Decreased volume by {percent}%")
+        except sr.UnknownValueError:
+            print("Sorry, I didn't understand that")
         else:
             talk('Please say the command again.')
 
